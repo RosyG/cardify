c463d70db74ff95d0de27bf222d3880ec1c12b15
RosyG <rmga2106@gmail.com>
2018-02-27 23:55:09 -0600

    Accediendo y guardando los atributos alt y src de cada imagen

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
+
+let gallery = document.getElementsByClassName('gallery');
+let arrayGallery = gallery[0]['childNodes'];
+let imagenArray = [];//Arreglo que en cada indice contiene toda la información de los tag img.
+for (let index in arrayGallery) {
+  if (index % 2 == 1) {
+    imagenArray.push(arrayGallery[index]);
+  }
+}
+
+$('#gallery').empty();//Borrando el contenedor una vez que la información anterior ya se encuentra almanecenada en las variables.
+//Esto es para que se puedan escribir los nuevos nodos de esa section.
+//Accediendo al atributos de cada imagen y guardandola en un arreglo.
+for (let content in imagenArray) {
+  let altText = imagenArray[content].alt; //Accediendo al atributo alt.
+  let srcText = imagenArray[content].currentSrc; //Accediendo al src.
+  let father = document.querySelector('#gallery');
+  father =  `<figure>
+                <img src="${srcText}" alt="${altText}">
+                <figcaption>${altText}</figcaption>
+            </figure>`;
+  console.log(altText);
+  console.log(srcText);
+}
+
+
 /*
-var img = new Image();
-img.src = '#';
-*/
-//`Hola ${ a + b } mundo ${ a * b}`
-//var containerGallery = document.querySelector('#prueba');
-/*
-var padre = hoja_a_eliminar.parentNode;
-padre.removeChild(hoja_a_eliminar);
-*/
-/*let title = 123;
-let title = 'lo que hay'
 let elementHTML = `<p title="${title}">Hello ${you}!</p>`
 */
-//let gallery = document.querySelector('#gallery');
-let gallery = document.getElementsByClassName('gallery');
-console.log(gallery[0]['childNodes']);
